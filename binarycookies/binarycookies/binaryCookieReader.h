4625d11b9fa5e864ab640ecaaac78a7bda1c9bb3
Чайка <mellanie335@gmail.com>
2016-03-22 23:27:26 +0900

    create class binaryCookieReader.

---
+++ binarycookies/binarycookies/binaryCookieReader.h
@@ -0,0 +1,13 @@
+//
+//  binaryCookieReader.h
+//  binarycookies
+//
+//  Created by Чайка on 3/22/16.
+//  Copyright © 2016 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface binaryCookieReader : NSObject
+
+@end
---
+++ binarycookies/binarycookies/binaryCookieReader.m
@@ -0,0 +1,13 @@
+//
+//  binaryCookieReader.m
+//  binarycookies
+//
+//  Created by Чайка on 3/22/16.
+//  Copyright © 2016 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "binaryCookieReader.h"
+
+@implementation binaryCookieReader
+
+@end
